4f37d8fd304b335c4f1f5844693a96369a4f69d9
attila.farago.hu <attila.farago.hu@gmail.com>
2018-10-10 21:26:08 +0200

    Initial commit - copy from GitLab

---
+++ js/scrolling-nav.js
@@ -0,0 +1,32 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+	var scrollOffset = 64;
+	
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - (scrollOffset-5)) //scrollspy should detect that is already in the new anchor
+        }, 1000, "easeInOutQuart");
+        history.pushState({}, document.title, this.hash);
+        return false;
+      }
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: scrollOffset
+  });
+
+})(jQuery); // End of use strict
